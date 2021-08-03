7bfb2368e0d1fe273b147b7f18c4c30160d9a6ba
ÁLVARO MEDINA <alvaro.e.medina.a@gmail.com>
2021-08-03 17:06:59 -0400

    Segundo progresoahora si

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,3 +1,14 @@
 class QuestionsController < ApplicationController
-  def ask; end
+  def ask
+  end
+  def answer
+  @answer =''
+  if  params[:question] == "i am going to work right now!"
+      @answer ="Great!"
+    elsif  params[:question].end_with?("?")
+      @answer ="Silly question, get dressed and go to work!"
+    else
+      @answer ="I don't care, get dressed and go to work!"
+    end
+  end
 end
